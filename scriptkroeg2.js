5e078afde27d37f2b794f94467f87ccab410a721
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-18 11:09:19 +0200

    Add files via upload

---
+++ scriptkroeg2.js
@@ -0,0 +1,66 @@
+const age=16;
+
+
+    if (age>=18) { console.log ("je mag naar binnen")
+        
+    }  else { console.log ("sorry, alleen 18+")
+        
+    }
+
+
+const isFem = false;
+
+if (isFem) { console.log ("je mag naar binnen")
+    
+} else 
+   {
+       
+    console.log ("sorry, ladies only vanavond")
+}
+
+const driverStatus="bobs"
+
+
+if (driverStatus==="bob") {console.log ("je mag rijden")
+    
+} else; {console.log ("bestel een taxi")
+    
+}
+
+const myAge=33;
+
+if (myAge>=18 && myAge<=25) {
+    console.log ("je krijgt 25% korting!")
+
+} else {console.log ("helaas betaal je de volle mep")}
+
+const myName="Bram";
+
+if (myName==="Sarah" || myName==="Bram") {
+    
+    console.log ("je krijgt een gratis biertje")}
+
+    else {console.log ("jammerdebammer geen gratis bier")}
+
+    /*bij een bestelling van 25 euro of meer krijgt de klant gratis (vega)bitterballen
+    bij een bestelling van 50 euro of meer krijgt de klant gratis nachos
+    bij een bestelling van 100 euro of meer krijgt de klant een flesje champagne*/
+   
+const totalAmount=10;
+
+if (totalAmount>=25 && totalAmount<=50) {
+    console.log ("gratis (vega)bitterballen!")
+}
+
+else if (totalAmount>=50 && totalAmount<=100) {
+    console.log ("gratis portie nachos!")
+}
+
+else if (totalAmount>=100) {
+    console.log ("gratis flesje champagne!")
+}
+
+else {
+    console.log ("jammerdebammer!")
+}
+
